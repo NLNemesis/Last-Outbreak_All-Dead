8f462adb1787a5f48817269a27f32bc8063ec428
IndieTeacher <UnitysEducation@gmail.com>
2025-09-26 18:41:18 +0300

    Changes

---
+++ Last-Outbreak_All-Dead/Assets/Player/PlayerMovement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    #region Variables
+    [Header("Controller")]
+    public bool Freezed;
+    public bool IsMoving;
+    [Header("Stats")]
+    public float MoveSpeed;
+    public float RotateSpeed;
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
+        {
+            IsMoving = true;
+            RotateSpeed = Input.GetAxisRaw("Horizontal") * Time.deltaTime * 150;
+            MoveSpeed = Input.GetAxisRaw("Vertical") * Time.deltaTime * 4;
+            this.gameObject.transform.Rotate(0, RotateSpeed, 0);
+            this.gameObject.transform.Translate(0, 0, MoveSpeed);
+        }
+        else
+        {
+            IsMoving = false;
+        }
+    }
+}
