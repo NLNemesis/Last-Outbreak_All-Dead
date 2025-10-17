b388649cef9708877c7e7400427e4bfb66149efb
IndieTeacher <UnitysEducation@gmail.com>
2025-10-17 20:40:43 +0300

    Changes

--- Last-Outbreak_All-Dead/Assets/Prefabs/Items/PickUpItem.cs
+++ Last-Outbreak_All-Dead/Assets/Prefabs/Items/PickUpItem.cs
@@ -7,12 +7,6 @@ using UnityEngine;
 public class PickUpItem : MonoBehaviour
 {
     #region Variables
-    public string SelectedItem;
-    public GameObject[] ItemObject;
-    public Sprite[] ItemIcon;
-    private string name;
-    private Sprite icon;
-
     [Header("Controller")]
     public bool GroundItem;
     private bool CanInteract;
@@ -20,25 +14,12 @@ public class PickUpItem : MonoBehaviour
     [Header("References")]
     private PlayerMovement PM;
     private Animator animator;
-    private Inventory inventory;
     #endregion
     // Start is called before the first frame update
     void Start()
     {
         PM = FindObjectOfType<PlayerMovement>();
         animator = FindObjectOfType<PlayerMovement>().GetComponent<Animator>();
-        inventory = FindObjectOfType<Inventory>();
-        for (int i = 0; i < ItemObject.Length; i++)
-        {
-            if (ItemObject[i].name == SelectedItem)
-            {
-                name = SelectedItem;
-                icon = ItemIcon[i];
-                ItemObject[i].SetActive(true);
-            }
-            else
-                ItemObject[i].SetActive(false);
-        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -63,12 +44,9 @@ public class PickUpItem : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.E) && CanInteract)
         {
             PM.freeze = true;
-            inventory.AddItem(name, icon);
             CanInteract = false;
-            if (GroundItem)
-                animator.Play("Pick_Down");
-            else
-                animator.Play("Pick_Up");
+            if (GroundItem) animator.Play("Pick_Down");
+            else animator.Play("Pick_Up");
             this.gameObject.SetActive(false);
         }
     }
