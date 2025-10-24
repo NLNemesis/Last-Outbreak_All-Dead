8b9785497546b2293064f20cc729442f925506a9
IndieTeacher <UnitysEducation@gmail.com>
2025-10-24 18:54:09 +0300

    Reset

---
+++ Last-Outbreak_All-Dead/Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    #region Variables
+    private int UIOpened;
+    public GameObject InventoryUI;
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (UIOpened == 0 || UIOpened == 2)
+            InventoryHandle();
+    }
+
+    #region Inventory Handle
+    void InventoryHandle()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            if (InventoryUI.activeSelf == false)
+            {
+                UIOpened = 2;
+                InventoryUI.SetActive(true);
+            }
+            else
+            {
+                InventoryUI.SetActive(false);
+                UIOpened = 0;
+            }
+        }
+    }
+    #endregion
+}
