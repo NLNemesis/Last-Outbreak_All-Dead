16487ec01383357ab2b28536e913b7b840fdb150
IndieTeacher <UnitysEducation@gmail.com>
2025-11-01 09:59:17 +0200

    Reset

--- Last-Outbreak_All-Dead/Assets/Player/Inventory.cs
+++ Last-Outbreak_All-Dead/Assets/Player/Inventory.cs
@@ -10,22 +10,11 @@ public class Inventory : MonoBehaviour
     [Header("Holder")]
     public Image[] SlotImage;
     public string[] SlotName;
-    public Button[] SlotButton;
     private int SlotAvailable;
-
-    [Header("Selection Panel")]
-    public GameObject[] ExamineObjectUI;
-    private int FirstSlot;
-    private int SecondSlot;
-    private bool CombinationOn;
 
     [Header("References")]
     private PlayerMovement PM;
     public GameObject SelectionPanel;
-
-    [Header("Combined Items")]
-    public string[] CombinedItemName;
-    public Sprite[] CombinedItemIcon;
     #endregion
     // Start is called before the first frame update
     void Start()
@@ -49,8 +38,6 @@ public class Inventory : MonoBehaviour
             {
                 SlotName[i] = name;
                 SlotImage[i].sprite = icon;
-                SlotImage[i].color = new Color(255, 255, 255, 255);
-                SlotButton[i].enabled = true;
                 SlotAvailable--;
                 break;
             }
@@ -64,8 +51,6 @@ public class Inventory : MonoBehaviour
             if (SlotName[i] == name)
             {
                 SlotName[i] = "Empty";
-                SlotImage[i].color = new Color(255, 255, 255, 0);
-                SlotButton[i].enabled = false;
                 SlotAvailable++;
                 break;
             }
@@ -75,88 +60,8 @@ public class Inventory : MonoBehaviour
     public void RemoveSpecificSlot(int slotID)
     {
         SlotName[slotID] = "Empty";
-        SlotImage[slotID].color = new Color(255, 255, 255, 0);
-        SlotButton[slotID].enabled = false;
         SlotAvailable++;
     }
     #endregion
 
-    #region Selection Panel
-    public void SelectSlot(int slotID)
-    {
-        if (!CombinationOn)
-            FirstSlot = slotID;
-        else
-        {
-            SecondSlot = slotID;
-            CombineItem();
-        }
-    }
-
-    public void UseItem()
-    {
-        if (SlotName[FirstSlot] == "First_Aid")
-        {
-            //Heal Player
-            RemoveSpecificSlot(FirstSlot);
-        }
-        else if (SlotName[FirstSlot] == "Green_Herb")
-        {
-            //Heal Player
-            RemoveSpecificSlot(FirstSlot);
-        }
-    }
-
-    public void ExamineItem()
-    {
-        for (int i = 0; i < ExamineObjectUI.Length; i++)
-        {
-            if (SlotName[FirstSlot] ==  ExamineObjectUI[i].name)
-                ExamineObjectUI[i].SetActive(true);
-            else
-                ExamineObjectUI[i].SetActive(false);
-        }
-    }
-
-    public void ToggleCombination()
-    {
-        CombinationOn = true;
-    }
-
-    public void CombineItem()
-    {
-        if ((SlotName[FirstSlot] == "Green_Herb" && SlotName[SecondSlot] == "Blue_Herb")
-          ||(SlotName[FirstSlot] == "Blue_Herb" && SlotName[SecondSlot] == "Green_Herb"))
-        {
-            RemoveSpecificSlot(FirstSlot);
-            RemoveSpecificSlot(SecondSlot);
-            AddItem(CombinedItemName[0], CombinedItemIcon[0]);
-            CombinationOn = false;
-            SelectionPanel.SetActive(false);
-        }
-        else if ((SlotName[FirstSlot] == "Green_Herb" && SlotName[SecondSlot] == "Red_Herb")
-          || (SlotName[FirstSlot] == "Red_Herb" && SlotName[SecondSlot] == "Green_Herb"))
-        {
-            RemoveSpecificSlot(FirstSlot);
-            RemoveSpecificSlot(SecondSlot);
-            AddItem(CombinedItemName[1], CombinedItemIcon[1]);
-            CombinationOn = false;
-            SelectionPanel.SetActive(false);
-        }
-        else if ((SlotName[FirstSlot] == "Green_Herb" && SlotName[SecondSlot] == "Green_Herb")
-           || (SlotName[FirstSlot] == "Green_Herb" && SlotName[SecondSlot] == "Green_Herb"))
-        {
-            RemoveSpecificSlot(FirstSlot);
-            RemoveSpecificSlot(SecondSlot);
-            AddItem(CombinedItemName[2], CombinedItemIcon[2]);
-            CombinationOn = false;
-            SelectionPanel.SetActive(false);
-        }
-    }
-
-    public void TrashItem()
-    {
-        RemoveSpecificSlot(FirstSlot);
-    }
-    #endregion
 }
