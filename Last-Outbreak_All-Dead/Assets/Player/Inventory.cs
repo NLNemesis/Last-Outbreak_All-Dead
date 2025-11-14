d748a2ce817d2eb6b5b8597b04aa5527201a310a
IndieTeacher <UnitysEducation@gmail.com>
2025-11-14 19:14:15 +0200

    Reset

--- Last-Outbreak_All-Dead/Assets/Player/Inventory.cs
+++ Last-Outbreak_All-Dead/Assets/Player/Inventory.cs
@@ -10,11 +10,22 @@ public class Inventory : MonoBehaviour
     [Header("Holder")]
     public Image[] SlotImage;
     public string[] SlotName;
+    public Button[] SlotButton;
     private int SlotAvailable;
+
+    [Header("Selection Panel")]
+    public GameObject[] ExamineObjectUI;
+    private bool CombinationOn;
+    private int FirstSlot;
+    private int SecondSlot;
 
     [Header("References")]
     private PlayerMovement PM;
     public GameObject SelectionPanel;
+
+    [Header("Mixed Items")]
+    public string[] MixedItemName;
+    public Sprite[] MixedItemIcon;
     #endregion
     // Start is called before the first frame update
     void Start()
@@ -38,6 +49,8 @@ public class Inventory : MonoBehaviour
             {
                 SlotName[i] = name;
                 SlotImage[i].sprite = icon;
+                SlotImage[i].color = new Color(255, 255, 255, 255);
+                SlotButton[i].enabled = true;
                 SlotAvailable--;
                 break;
             }
@@ -51,6 +64,8 @@ public class Inventory : MonoBehaviour
             if (SlotName[i] == name)
             {
                 SlotName[i] = "Empty";
+                SlotImage[i].color = new Color(255, 255, 255, 0);
+                SlotButton[i].enabled = false;
                 SlotAvailable++;
                 break;
             }
@@ -60,8 +75,100 @@ public class Inventory : MonoBehaviour
     public void RemoveSpecificSlot(int slotID)
     {
         SlotName[slotID] = "Empty";
+        SlotImage[slotID].color = new Color(255, 255, 255, 0);
+        SlotButton[slotID].enabled = false;
         SlotAvailable++;
     }
     #endregion
 
+    #region Select Slot Handler
+    public void SelectSlot(int slotID)
+    {
+        if (!CombinationOn)
+        {
+            FirstSlot = slotID;
+            SelectionPanel.SetActive(true);
+        }
+        else
+        {
+            SecondSlot = slotID;
+            Combine();
+            SelectionPanel.SetActive(false);
+        }
+    }
+    #endregion
+
+    #region Use Handler
+    public void UseSlot()
+    {
+        if (SlotName[FirstSlot] == "First_Aid")
+        {
+            RemoveSpecificSlot(FirstSlot);
+            //Heal Player
+        }
+        else if (SlotName[FirstSlot] == "Green_Herb")
+        {
+            RemoveSpecificSlot(FirstSlot);
+            //Heal Player
+        }
+        SelectionPanel.SetActive(false);
+    }
+    #endregion
+
+    #region Examine Handler
+    public void ExamineSlot()
+    {
+        for (int i = 0; i < ExamineObjectUI.Length; i++)
+        {
+            if (ExamineObjectUI[i].name == SlotName[FirstSlot])
+            {
+                ExamineObjectUI[i].SetActive(true);
+                break;
+            }
+        }
+        SelectionPanel.SetActive(false);
+    }
+    #endregion
+
+    #region Combine Handler
+    public void ToggleCombination()
+    {
+        CombinationOn = true;
+    }
+
+    void Combine()
+    {
+        if ((SlotName[FirstSlot] == "Green_Herb" && SlotName[SecondSlot] == "Blue_Herb") 
+        || (SlotName[FirstSlot] == "Blue_Herb" && SlotName[SecondSlot] == "Green_Herb"))
+        {
+            RemoveSpecificSlot(FirstSlot);
+            RemoveSpecificSlot(SecondSlot);
+            AddItem(MixedItemName[0], MixedItemIcon[0]);
+        }
+        else if ((SlotName[FirstSlot] == "Green_Herb" && SlotName[SecondSlot] == "Red_Herb")
+        || (SlotName[FirstSlot] == "Red_Herb" && SlotName[SecondSlot] == "Green_Herb"))
+        {
+            RemoveSpecificSlot(FirstSlot);
+            RemoveSpecificSlot(SecondSlot);
+            AddItem(MixedItemName[1], MixedItemIcon[1]);
+        }
+        else if ((SlotName[FirstSlot] == "Green_Herb" && SlotName[SecondSlot] == "Green_Herb")
+        || (SlotName[FirstSlot] == "Green_Herb" && SlotName[SecondSlot] == "Green_Herb"))
+        {
+            RemoveSpecificSlot(FirstSlot);
+            RemoveSpecificSlot(SecondSlot);
+            AddItem(MixedItemName[1], MixedItemIcon[1]);
+        }
+
+        CombinationOn = false;
+        SelectionPanel.SetActive(false);
+    }
+    #endregion
+
+    #region Trash Handler
+    public void TrashSlot()
+    {
+        RemoveSpecificSlot(FirstSlot);
+    }
+    #endregion
 }
