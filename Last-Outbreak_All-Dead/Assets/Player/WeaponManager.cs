a839f2096e383fb49f3b9ff9ddac7c344401e2eb
IndieTeacher <UnitysEducation@gmail.com>
2025-11-29 11:27:54 +0200

    Reset

---
+++ Last-Outbreak_All-Dead/Assets/Player/WeaponManager.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour
+{
+    #region Variables
+    [Header("Controller")]
+    public int gunID;
+    public RuntimeAnimatorController controller;
+
+    [Header("References")]
+    private PlayerMovement pm;
+    private Animator animator;
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        pm = GetComponentInParent<PlayerMovement>();
+        animator = GetComponentInParent<Animator>();
+        animator.runtimeAnimatorController = controller;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleMovement();
+    }
+
+    #region Handle Movement
+    private bool aiming;
+    private float horizontalMove;
+    private float verticalMove;
+
+    void HandleMovement()
+    {
+        //Start Aiming
+        if (Input.GetMouseButton(1) && !aiming)
+        {
+            aiming = true;
+            animator.SetBool("aiming", true);
+            animator.SetTrigger("aim");
+        }
+
+        //While Aiming
+        if (aiming)
+        {
+            //Rotate Player
+            horizontalMove = Input.GetAxisRaw("Horizontal") * Time.deltaTime * pm.rotateSpeed;
+            pm.gameObject.transform.Rotate(0, horizontalMove, 0);
+            //Move Player
+            verticalMove = Input.GetAxisRaw("Vertical");
+            
+        }
+
+        //Stop Aiming
+        if (Input.GetMouseButtonUp(1) && aiming)
+        {
+            aiming = false;
+            animator.SetBool("aiming", false);
+            animator.SetTrigger("aim");
+        }
+    }
+    #endregion
+}
