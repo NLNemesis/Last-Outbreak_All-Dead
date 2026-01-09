c9ac45972c557140896bf6fc8e7b7ae1981505a5
IndieTeacher <UnitysEducation@gmail.com>
2026-01-09 19:47:34 +0200

    Reset

--- Last-Outbreak_All-Dead/Assets/Prefabs/Enemies/Behaviour/AIDetect.cs
+++ Last-Outbreak_All-Dead/Assets/Prefabs/Enemies/Behaviour/AIDetect.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class AIDetect : MonoBehaviour
 {
+    #region Variables
+    [Header("Variables")]
+    public bool canAttack;
+    public bool dealDamage;
+
+    [Header("References")]
+    private AIMove aiMove;
+    #endregion
     // Start is called before the first frame update
     void Start()
     {
-        
+        aiMove = GetComponentInParent<AIMove>();   
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.name == "Player" && canAttack)
+            aiMove.Attack();
+        else if (other.name == "Player" && dealDamage)
+            aiMove.DealDamage();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.name == "Player" && canAttack)
+            aiMove.Attack();
+        else if (other.name == "Player" && dealDamage)
+            aiMove.DealDamage();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "Player" && canAttack)
+            aiMove.Attack();
+        else if (other.name == "Player" && dealDamage)
+            aiMove.DealDamage();
     }
 }
